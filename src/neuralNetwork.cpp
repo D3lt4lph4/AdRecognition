de0d2f04127036a1cac2e6ba489e67eaa63f64b0
benjamin deguerre <benjamin.deguerre@actemium.com>
2019-06-02 23:18:11 +0200

    working version of the neural network training

--- src/neuralNetwork.cpp
+++ src/neuralNetwork.cpp
@@ -2,11 +2,8 @@
 // usage: prog trainingData_file testingData_file
 
 // For use with testing/training datasets of the same format as: ad_cranfield.data
-
-// for simple test of data reading run as "reader ad_cranfield.data ad_cranfield.data"
-// and set defintion below to 1 to print out input data
-
 /******************************************************************************/
+
 #include <cv.h>
 #include <ml.h>
 #include <iostream>
@@ -18,16 +15,15 @@ using namespace cv::ml;
 #include <stdio.h>
 
 #define ATTRIBUTES_PER_SAMPLE 1558
-
 #define NUMBER_OF_CLASSES 2
 
 int read_data_from_csv(const char* filename, Mat &data, Mat &classes) {
-  int n_samples = 0;
 
+  int n_samples = 0;
+  float value;
   std::string line;
   std::ifstream my_file(filename);
   std::string::size_type sz;
-  float value;
 
   // Getting the number of lines
   while (std::getline(my_file, line))
@@ -52,8 +48,10 @@ int read_data_from_csv(const char* filename, Mat &data, Mat &classes) {
         if (line.compare("ad.") == 0) {
           // adverts are class 1
           classes.at<float>(line_nb, 0) = 1.0;
+          classes.at<float>(line_nb, 1) = 0.0;
         } else if (line.compare("nonad.") == 0) {
           // non adverts are class 0
+          classes.at<float>(line_nb, 0) = 0.0;
           classes.at<float>(line_nb, 1) = 1.0;
         }
       } else {
@@ -65,7 +63,7 @@ int read_data_from_csv(const char* filename, Mat &data, Mat &classes) {
       }
     }
   }
-  return 1;  // all OK
+  return 1;
 }
 
 void selectNFold(Mat data, Mat classes, Mat &trainingData, Mat &trainingClassifications, Mat &validationData, Mat &validationClassifications, int sampleNumber, int nFolds) {
@@ -80,23 +78,17 @@ void selectNFold(Mat data, Mat classes, Mat &trainingData, Mat &trainingClassifi
   trainingClassifications.create(nSamplesPerFold * (nFolds - 1), NUMBER_OF_CLASSES, CV_32FC1);
 
   //copy data for validation sample
-  data(Rect(0,nSamplesPerFold * (sampleNumber - 1),ATTRIBUTES_PER_SAMPLE, nSamplesPerFold )).copyTo(validationData);
-  classes(Rect(0,nSamplesPerFold * (sampleNumber - 1), NUMBER_OF_CLASSES, nSamplesPerFold )).copyTo(validationClassifications);
+  data(Rect(0,nSamplesPerFold * (sampleNumber - 1), ATTRIBUTES_PER_SAMPLE, nSamplesPerFold)).copyTo(validationData);
+  classes(Rect(0,nSamplesPerFold * (sampleNumber - 1), NUMBER_OF_CLASSES, nSamplesPerFold)).copyTo(validationClassifications);
 
   if (sampleNumber == 1) {
-
     data(Rect(0, nSamplesPerFold, ATTRIBUTES_PER_SAMPLE, nSamplesPerFold * (nFolds - 1))).copyTo(trainingData);
     classes(Rect(0,nSamplesPerFold, NUMBER_OF_CLASSES, nSamplesPerFold * (nFolds - 1))).copyTo(trainingClassifications);
 
   } else if (sampleNumber == nFolds) {
-
     data(Rect(0, 0, ATTRIBUTES_PER_SAMPLE, nSamplesPerFold * (nFolds - 1))).copyTo(trainingData);
     classes(Rect(0, 0, NUMBER_OF_CLASSES, nSamplesPerFold * (nFolds - 1))).copyTo(trainingClassifications);
-
   } else {
-
-    trainingData.resize(nSamplesPerFold * (nFolds - 1));
-    trainingClassifications.resize(nSamplesPerFold * (nFolds - 1));
 
     //get first part of the dataset
     data(Rect(0, 0, ATTRIBUTES_PER_SAMPLE, nSamplesPerFold * (sampleNumber - 1))).copyTo(trainingData(Rect(0, 0, ATTRIBUTES_PER_SAMPLE, nSamplesPerFold * (sampleNumber - 1))));
@@ -107,8 +99,6 @@ void selectNFold(Mat data, Mat classes, Mat &trainingData, Mat &trainingClassifi
   }
 }
 
-/******************************************************************************/
-
 int main( int argc, char** argv ) {
 
   // define training data storage matrices (one for attribute examples, one
@@ -116,9 +106,11 @@ int main( int argc, char** argv ) {
   Mat trainingData = Mat();
   Mat trainingClassifications = Mat();
 
-  //define testing data storage matrices
   Mat validationData = Mat();
   Mat validationClassifications = Mat();
+
+  Mat testData = Mat();
+  Mat testClassifications = Mat();
 
   Mat data = Mat();
   Mat dataClassification = Mat();
@@ -129,27 +121,24 @@ int main( int argc, char** argv ) {
   Point max_loc = Point(0,0);
 
   //Creating matrix for validation
-  Mat validationSample;
+  Mat validationSample, testSample;
   int correctClass = 0, wrongClass = 0, falsePositives[NUMBER_OF_CLASSES] = {0,0};
 
-  //Vector for mean and std values
-  std::vector<float> means, std;
-
-  //Setting the variable for parameter otimization
+  //Setting the variable for parameter optimization
   int numberOfIterations = 10, paramMin = 10, step = 20, nFolds = 5, bestParam = paramMin;
 
   //Error to be minimized
   double errorMin = 100;
 
   Scalar meanError;
+  Ptr<ANN_MLP> nnetwork;
 
   Mat error = Mat(nFolds, numberOfIterations, CV_32FC1);
   //Setting precision for display
   std::cout.precision(4);
 
-  //Creating files to write the data to, will then be used for modelTester (model only, no training)
-  std::ofstream csvFile;
-  std::string csvName = "data/csv/", file = "data/models/";
+  //Creating file to write the data to, will then be used for modelTester (model only, no training)
+  std::string model_file = "data/models/";
 
   //Try to read data from the input file
   if (read_data_from_csv(argv[1], data, dataClassification)) {
@@ -175,7 +164,7 @@ int main( int argc, char** argv ) {
           falsePositives[j] = 0;
         }
 
-        //Setting the layers, the number of neuron will ba what we try to optimize
+        //Setting the layers, the number of neuron will be what we try to optimize
         int layers_d[] = { ATTRIBUTES_PER_SAMPLE, paramMin + i * step, NUMBER_OF_CLASSES};
         Mat layers = Mat(1,3,CV_32SC1);
         layers.at<int>(0,0) = layers_d[0];
@@ -183,12 +172,12 @@ int main( int argc, char** argv ) {
         layers.at<int>(0,2) = layers_d[2];
 
         //We create the classifier
-        Ptr<ANN_MLP> nnetwork = ANN_MLP::create();
+        nnetwork = ANN_MLP::create();
 
         nnetwork->setLayerSizes(layers);
-        nnetwork->setActivationFunction(ANN_MLP::SIGMOID_SYM, 1, 1);
-        nnetwork->setTermCriteria(TermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 1000, 0.01));
-        nnetwork->setTrainMethod(ANN_MLP::BACKPROP);
+        nnetwork->setActivationFunction(ANN_MLP::SIGMOID_SYM, 1.0, 1.0);
+        nnetwork->setTermCriteria(TermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 1000, 0.0001));
+        nnetwork->setTrainMethod(ANN_MLP::BACKPROP, 0.0001);
 
         // train the neural network (using training data)
         std::cout << "Training iteration for the classifier : " << i + 1 << std::endl;
@@ -210,6 +199,8 @@ int main( int argc, char** argv ) {
 
           minMaxLoc(classificationResult, 0, 0, 0, &max_loc);
 
+          std::cout << classificationResult << " " << validationClassifications.at<float>(tsample, 0) << " " << validationClassifications.at<float>(tsample, 1)  << std::endl;
+
           //getting wrong and correct classification
           if (validationClassifications.at<float>(tsample, max_loc.x) == 1) {
             correctClass++;
@@ -228,60 +219,73 @@ int main( int argc, char** argv ) {
       }
     }
 
-
-    if (argc == 3) {
-      //File for the graphic
-      csvName.append(argv[2]);
-      csvName.append(".csv");
-      csvFile.open(csvName, std::ofstream::out | std::ofstream::trunc);
-    }
-
     for (int i = 0; i < numberOfIterations ; i++) {
       meanError = mean(error.col(i));
       if (errorMin > meanError[0]) {
         errorMin = meanError[0];
         bestParam = paramMin + i * step;
       }
-
-      if (argc == 3) {
-        csvFile << paramMin + i * step;
-        csvFile << ",";
-        csvFile << meanError[0];
-        csvFile << ";\n";
-      }
     }
 
     std::cout << "The best value for the parameter is : " << bestParam << std::endl;
     std::cout << "The error for this parameter is : " << errorMin << std::endl;
 
+    correctClass = 0, wrongClass = 0;
+    falsePositives[0] = 0;
+    falsePositives[1] = 0;
+
+    read_data_from_csv(argv[2], testData, testClassifications);
+
+    int layers_bi[] = { ATTRIBUTES_PER_SAMPLE, bestParam, NUMBER_OF_CLASSES};
+    Mat layers_b = Mat(1,3,CV_32SC1);
+    layers_b.at<int>(0,0) = layers_bi[0];
+    layers_b.at<int>(0,1) = layers_bi[1];
+    layers_b.at<int>(0,2) = layers_bi[2];
+    //We create the classifier
+    nnetwork = ANN_MLP::create();
+
+    nnetwork->setLayerSizes(layers_b);
+    nnetwork->setActivationFunction(ANN_MLP::SIGMOID_SYM, 1.0, 1.0);
+    nnetwork->setTermCriteria(TermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 1000, 0.0001));
+    nnetwork->setTrainMethod(ANN_MLP::BACKPROP, 0.0001);
+    
+    Ptr<TrainData> nData = TrainData::create(testData, SampleTypes::ROW_SAMPLE, testClassifications);
+
+    std::cout << "Training the model with the best parameter on the whole dataset." << std::endl;
+
+    nnetwork->train(nData);
+
+    for (int tsample = 0; tsample < testData.rows; tsample++) {
+
+      // extract a row from the validation matrix
+      testSample = testData.row(tsample);
+
+      // run neural network prediction
+      nnetwork->predict(testSample, classificationResult);
+
+      minMaxLoc(classificationResult, 0, 0, 0, &max_loc);
+
+      //getting wrong and correct classification
+      if (testClassifications.at<float>(tsample, max_loc.x) == 1) {
+        correctClass++;
+      } else {
+        wrongClass++;
+        falsePositives[(int) max_loc.x]++;
+      }
+    }
+
+    std::cout << "Result on test set :\n\tCorrect classification: " << correctClass << ", " << (double)correctClass*100/testData.rows << "%\n\tWrong classifications: " << wrongClass << " " << (double) wrongClass*100/testData.rows << "%" << std::endl;
+
+    std::cout << "\tClass nonad false postives : " << (double)falsePositives[1]*100/testData.rows << std::endl;
+    std::cout << "\tClass ad false postives : " << (double)falsePositives[0]*100/testData.rows << std::endl;
+
     //If model to save
-    if (argc == 3) {
+    if (argc == 4) {
       //File path for model
-      file.append(argv[2]);
-      file.append(".xml");
-      //Calculating the model with the best parameter.
-      int layers_d[] = { ATTRIBUTES_PER_SAMPLE, bestParam, NUMBER_OF_CLASSES};
-      Mat layers = Mat(1,3,CV_32SC1);
-      layers.at<int>(0,0) = layers_d[0];
-      layers.at<int>(0,1) = layers_d[1];
-      layers.at<int>(0,2) = layers_d[2];
+      model_file.append(argv[3]);
+      model_file.append(".xml");
+      nnetwork->save(model_file.c_str());
 
-      //We create the classifier
-      Ptr<ANN_MLP> nnetwork = ANN_MLP::create();
-
-      nnetwork->setLayerSizes(layers);
-      nnetwork->setActivationFunction(ANN_MLP::SIGMOID_SYM, 0.6, 1);
-      nnetwork->setTermCriteria(TermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 1000, 0.000001));
-      nnetwork->setTrainMethod(ANN_MLP::BACKPROP, 0.1, 0.1);
-      
-      Ptr<TrainData> nData = TrainData::create(data, SampleTypes::ROW_SAMPLE, dataClassification);
-      std::cout << "Training the model with the best parameter on the whole dataset." << std::endl;
-
-      nnetwork->train(nData);
-
-      nnetwork->save(file.c_str());
-
-      csvFile.close();
     }
     return 0;
   }
